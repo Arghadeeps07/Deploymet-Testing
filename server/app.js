8a4d1214d19225ee5ad27a1136d6dfb08440fdfd
Arghadeep <arghadeeps07@gmail.com>
2025-08-02 22:33:35 +0530

    Backend update

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const PORT = 5000;
 app.use(cors());
 
 app.get('/api/message', (req, res) => {
-  res.json({ message: 'Hello from backend!' });
+  res.json({ message: 'Hello I am Arghadeep' });
 });
 
 app.listen(PORT, () => {
